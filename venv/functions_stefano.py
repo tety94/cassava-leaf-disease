0f96a0ced07c3d8180664cb442b24282fe09d44d
callegarostefano <stefano.callegaro@h-en.me>
2020-12-21 23:14:11 +0100

    crop images

--- venv/functions_stefano.py
+++ venv/functions_stefano.py
@@ -6,6 +6,7 @@ import matplotlib.image as img
 import os
 from os import listdir
 from os.path import isfile, join
+from PIL import Image
 
 
 class JsonAnalizer:
@@ -68,7 +69,7 @@ def plotImagesByPaths(imagesNames, basePath, offset, nrows, ncols):
     while firstCont < cycles:
         figure, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, nrows * ncols), constrained_layout=True)
         cont = 0
-        for i in imagesNames[offset * nrows * ncols: (offset + 1) * nrows * ncols ]:
+        for i in imagesNames[offset * nrows * ncols: (offset + 1) * nrows * ncols]:
             im = img.imread(basePath + i)
             ax.ravel()[cont].imshow(im)
             ax.ravel()[cont].set_title(i)
@@ -77,3 +78,29 @@ def plotImagesByPaths(imagesNames, basePath, offset, nrows, ncols):
         plt.show()
         firstCont = firstCont + 1
         offset = offset + 1
+
+
+def cropAllImages(imagesList, basePath, savePath = '', new_width=500, new_height=500, show=True, save=True):
+    if(savePath == ''):
+        savePath = basePath
+
+    for image in imagesList:
+        cropImage(image, basePath, savePath = savePath, new_width=new_width, new_height=new_height, show=show, save=save)
+
+def cropImage(image, basePath, savePath, new_width=500, new_height=500, show=True, save=True):
+    if(savePath == ''):
+        savePath = basePath
+    im = Image.open(basePath + image)
+    width, height = im.size  # Get dimensions
+
+    left = (width - new_width) / 2
+    top = (height - new_height) / 2
+    right = (width + new_width) / 2
+    bottom = (height + new_height) / 2
+
+    # Crop the center of the image
+    im = im.crop((left, top, right, bottom))
+    if (show):
+        im.show()
+    if (save):
+        im.save(savePath + image, "JPEG")
--- venv/main_stefano.py
+++ venv/main_stefano.py
@@ -8,6 +8,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 DATA_PATH = dir_path + '/data/'
 TRAIN_PATH = DATA_PATH + '/train/'
+TRAIN_CROPPED_PATH = DATA_PATH + '/train_crop/'
 jsonPath = DATA_PATH + '/label_num_to_disease_map.json'
 csvPath = DATA_PATH + '/train.csv'
 
@@ -18,28 +19,10 @@ df = pandasUtils(csvPath)
 df.takeExistingImages(TRAIN_PATH)  # now the dataset has only existing images
 # df.printInfo()
 
-labels = [4]
+labels = [0]
 imagesPath = df.getImagesNamesByLabels(labels)
 
+# plotImagesByPaths(imagesNames=imagesPath, basePath=TRAIN_PATH, offset=0, nrows=3, ncols=4)
 
-plotImagesByPaths(imagesNames=imagesPath, basePath=TRAIN_PATH, offset=0, nrows=3, ncols=4)
+cropAllImages(imagesList = imagesPath, basePath = TRAIN_PATH, savePath = TRAIN_CROPPED_PATH, show=False)
 
-# def get_x(r):
-#     # print( r)
-#     return TRAIN_PATH
-#     return (Path(TRAIN_PATH) / r["image_id"])
-#
-#
-# def get_y(r):
-#     return (r["label"])
-#
-# fields = DataBlock(blocks=(ImageBlock, CategoryBlock),
-#                    get_items=get_x,
-#                    get_y=get_y,
-#                    splitter=RandomSplitter(valid_pct=0.2, seed=42),
-#                    item_tfms=RandomResizedCrop(224, min_scale=0.5),
-#                    batch_tfms=aug_transforms())
-#
-#
-#
-# dls = fields.dataloaders(TRAIN_PATH)
