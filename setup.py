c70ea6784291ffd11140c326ae5615b247428cb1
callegarostefano <stefano.callegaro@h-en.me>
2020-12-28 22:30:26 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cassava-leaf-disease',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/tety94/cassava-leaf-disease',
+    license='',
+    author='stefano',
+    author_email='callegarostefano@gmail.com',
+    description='Cassave Leaf, Kaggle competition'
+)
