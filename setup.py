4e0e151f088da426a1c104c70535eeaf027de84a
stefano94 <callegarostefano@gmail.com>
2020-12-31 16:25:21 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='cassava-leaf-disease',
+    name='cassava',
     version='0.0.1',
     packages=[''],
     url='https://github.com/tety94/cassava-leaf-disease',
