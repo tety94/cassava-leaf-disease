99034c60ee7b1a8d8ff7a497c47f25e98757647d
Davide Rigon <davide.rigon.dr@gmai.com>
2021-01-03 11:57:27 +0100

    Created the function ConfusionMatrix

--- cassava/functions_davide.py
+++ cassava/functions_davide.py
@@ -1,3 +1,31 @@
-from fastai import *
+from sklearn.metrics import confusion_matrix
 
-#t = fastai.data.core.
+def ConfusionMatrix(valid, model, bNormalize = True, bGradient = True):
+    """
+    :param valid: the validation set;
+    :param model: the model (use learn.model.eval() to get it);
+    :param bNormalize: a Boolean parameter to set the normalization (or not) of data;
+    :param bGradient: a Boolean parameter to set the Gradient (or not) in the return;
+    :return: a Pandas DataFrame with the Confusion Matrix.
+
+    """
+
+    label = []
+    pred = []
+
+    for i, j in valid:
+        pred.append(torch.argmax(model(i), axis=1).tolist())
+        label.append(j.tolist())
+
+    if bNormalize:
+        df = pd.DataFrame(confusion_matrix(np.array(list(itertools.chain.from_iterable(label))),
+                                                    np.array(list(itertools.chain.from_iterable(pred))),
+                                                    normalize="true")).mul(100).round(2)
+    else:
+        df = pd.DataFrame(confusion_matrix(np.array(list(itertools.chain.from_iterable(label))),
+                                           np.array(list(itertools.chain.from_iterable(pred)))))
+
+    if bGradient:
+        df = df.style.set_properties(**{"font-size": "10pt"}).background_gradient("Blues").format('{0:,.2f}%')
+
+    return df
